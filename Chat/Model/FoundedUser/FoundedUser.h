c1c4b0fdeccbc957e430bd51f9a38fe1f8093f23
maksdpua <maksdpua@gmail.com>
2015-11-14 14:58:32 +0200

    FoundedUser

---
+++ Chat/Model/FoundedUser/FoundedUser.h
@@ -0,0 +1,13 @@
+//
+//  FoundedUser.h
+//  Chat
+//
+//  Created by Maks on 11/14/15.
+//  Copyright © 2015 Maks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FoundedUser : NSObject
+
+@end
---
+++ Chat/Model/FoundedUser/FoundedUser.m
@@ -0,0 +1,13 @@
+//
+//  FoundedUser.m
+//  Chat
+//
+//  Created by Maks on 11/14/15.
+//  Copyright © 2015 Maks. All rights reserved.
+//
+
+#import "FoundedUser.h"
+
+@implementation FoundedUser
+
+@end
